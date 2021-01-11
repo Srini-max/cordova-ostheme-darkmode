2071edd359dc22e7f7585dfea679c809140da835
Srinivas <54628670+Srini-max@users.noreply.github.com>
2021-01-12 02:14:46 +0400

    Update OSTheme.java

--- src/android/OSTheme.java
+++ src/android/OSTheme.java
@@ -13,28 +13,30 @@ public class OSTheme extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) {
         if (action.equals("darkmode")) {
+            try {
+                int a = cordova.getActivity().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
 
-            int a = cordova.getActivity().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-
-            switch (a) {
-                case Configuration.UI_MODE_NIGHT_NO:
-                    Log.d(TAG, "Dark Mode: off");
-                    callbackContext.success("false");
-                    callbackContext.error("Error setting 'isDark' ");
-
-                    break;
-                    // Night mode is not active, we're in day time
-                case Configuration.UI_MODE_NIGHT_YES:
-                    Log.d(TAG, "Dark Mode: on");
-                    callbackContext.success("true");
-                    break;
-                    // Night mode is active, we're at night!
-                case Configuration.UI_MODE_NIGHT_UNDEFINED:
-                    // We don't know what mode we're in, assume notnight
-                    Log.d(TAG, "Dark Mode: undefined");
-                    callbackContext.success("false");
-                    callbackContext.error("Error setting 'isDark' ");
-                    break;
+                switch (a) {
+                    case Configuration.UI_MODE_NIGHT_NO:
+                        Log.d(TAG, "Dark Mode: off");
+                        callbackContext.success("false");
+                        break;
+                        // Night mode is not active, we're in day time
+                    case Configuration.UI_MODE_NIGHT_YES:
+                        Log.d(TAG, "Dark Mode: on");
+                        callbackContext.success("true");
+                        break;
+                        // Night mode is active, we're at night!
+                    case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                        // We don't know what mode we're in, assume notnight
+                        Log.d(TAG, "Dark Mode: undefined");
+                        callbackContext.success("false");
+                        break;
+                }
+            } catch (Settings.SettingNotFoundException e) {
+                Log.d(TAG, "Error finding setting night mode: " + e.getMessage());
+                Log.d(TAG, "Checking dark/night mode enabled status");
+                callbackContext.error("Error finding setting night mode: " + e.getMessage());
             }
             return true;
 
