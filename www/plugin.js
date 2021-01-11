f67f062d071773e0037983c7bbd87cacf5ab29fd
Srinivas <54628670+Srini-max@users.noreply.github.com>
2021-01-12 01:19:19 +0400

    Update plugin.js

--- www/plugin.js
+++ www/plugin.js
@@ -1,8 +1,13 @@
-module.exports = {
-    darkmode: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "GetOSTheme", "darkmode", []);
-    },
-    inversion: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "GetOSTheme", "inversion", []);
-    }	
-};
+cordova.addConstructor(function() {
+    function GetOSTheme() {
+
+    }
+
+    GetOSTheme.prototype.darkmode = function( base64, successCallback, errorCallback ){
+        cordova.exec(successCallback, errorCallback, "GetOSTheme", "darkmode", [base64]);
+    }
+
+   
+    window.GetOSTheme = new GetOSTheme()
+    return window.GetOSTheme
+});
