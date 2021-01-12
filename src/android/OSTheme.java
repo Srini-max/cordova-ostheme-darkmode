3e87c986e64b3b7b020f419e9940c9bcc621b873
Srinivas <54628670+Srini-max@users.noreply.github.com>
2021-01-12 16:22:30 +0400

    Update OSTheme.java

--- src/android/OSTheme.java
+++ src/android/OSTheme.java
@@ -40,30 +40,8 @@ public class OSTheme extends CordovaPlugin {
             }
             return true;
 
-        } else if (action.equals("inversion")) {
-
-            boolean isInversionEnabled = false;
-            int accessibilityEnabled = 0;
-            try {
-                accessibilityEnabled = Settings.Secure.getInt(cordova.getActivity().getContentResolver(),
-                    android.provider.Settings.Secure.ACCESSIBILITY_DISPLAY_INVERSION_ENABLED);
-            } catch (Settings.SettingNotFoundException e) {
-                Log.d(TAG, "Error finding setting ACCESSIBILITY_DISPLAY_INVERSION_ENABLED: " + e.getMessage());
-                Log.d(TAG, "Checking negative color enabled status");
-                final String SEM_HIGH_CONTRAST = "high_contrast";
-                accessibilityEnabled = Settings.System.getInt(cordova.getActivity().getContentResolver(), SEM_HIGH_CONTRAST, 0);
-            }
-            if (accessibilityEnabled == 1) {
-                Log.d(TAG, "inversion  or negative colour is enabled");
-                isInversionEnabled = true;
-            } else {
-                Log.d(TAG, "inversion  or negative colour is disabled");
-            }
-            callbackContext.success("" + isInversionEnabled);
-
-            return true;
-
         } else {
+            Log.d(TAG, "Wrong Function... nothing to detect");
             return false;
         }
     }
